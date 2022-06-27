c4969f28a67182c121c849693f1ace0e7cf5ebca
YutakaHub <92905563+YutakaHub@users.noreply.github.com>
2022-06-27 19:33:44 +0900

    make commpornents

--- App.jsx
+++ App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-//  import MemoListScreen from './src/screens/MemoListScreen';
+import MemoListScreen from './src/screens/MemoListScreen';
 //  import MemoEditScreen from './src/screens/MemoEditScreen';
 //  import MemoCreateScreen from './src/screens/MemoCreateScreen';
 //  import LoginScreen from './src/screens/LoginScreen';
 //  import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
+//  import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-  //  <MemoListScreen />
+    <MemoListScreen />
   //  <MemoDetailScreen />
   //  <MemoEditScreen />
   //  <MemoCreateScreen />
   //  <LoginScreen />
-    <SignUpScreen />
+  //  <SignUpScreen />
   );
 }
