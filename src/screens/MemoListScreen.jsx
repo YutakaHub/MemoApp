7487b142dee8e6603f1df14f45edf20f7ab1994d
YutakaHub <92905563+YutakaHub@users.noreply.github.com>
2022-07-05 20:46:21 +0900

    init MemoScreen create

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, SnapshotViewIOS } from 'react-native';
+import { View, StyleSheet, SnapshotViewIOS, Text } from 'react-native';
 import firebase from 'firebase';
 
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
+import Button from '../components/Button';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
@@ -41,6 +42,19 @@ export default function MemoListScreen(props) {
     return unsubscribe;
   },[]);
 
+  if (memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <View style={emptyStyles.inner}>
+          <Text sytel={emptyStyles.title}>最初のメモを作成してみよう！</Text>
+          <Button
+          style={emptyStyles.button}
+          label="作成する" onPress={() => {navigation.navigate('MemoCreate')}} />
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <MemoList memos = {memos} />
@@ -57,4 +71,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F0F4F8',
   },
+});
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
+  }
 });
